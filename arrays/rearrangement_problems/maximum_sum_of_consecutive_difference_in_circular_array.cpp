42e5d761dfe26b7d1fa208b3e7e4cbff45874d1f
raghav <raghav.alagh2015@vit.ac.in>
2017-12-21 20:47:21 +0530

    longest increasing subsequence done

---
+++ arrays/rearrangement_problems/maximum_sum_of_consecutive_difference_in_circular_array.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define bye return 0
+#define pb push_back
+#define mp make_pair
+#define mod(n) (n) % 1000000007
+#define e_val 2.718281828
+
+typedef long long int lli;
+typedef long long ll;
+typedef unsigned long long int ulli;
+typedef pair<lli,lli> plli;
+typedef vector<lli> vlli;
+typedef map<string,lli> mslli;
+typedef map<lli,lli> mlli;
+typedef vector<pair<lli,lli> > vplli;
+
+inline bool isPrime(lli n){
+	if (n <= 1)  {
+		return false;
+	}
+	if (n <= 3)  {
+		return true;
+	}
+
+	if (n%2 == 0 || n%3 == 0) {
+		return false;
+	}
+	
+	for (int i=5; i*i<=n; i=i+6){
+		if (n%i == 0 || n%(i+2) == 0){
+			return false;
+		}
+	}
+
+	return true;
+}
+
+inline bool isEven(lli x){
+	if(x&1) return false;
+	else return true;
+}
+
+inline int binSearch(int arr[],int val,int b){
+    int low=0,high=b-1,mid;
+    while(low<=high){
+        mid=(low+high)/2;
+        if(arr[mid]>val){
+            high=mid-1;
+        }
+        else if(arr[mid]<val){
+            low=mid+1;
+        }
+        else{
+            return 1;
+        }
+    }
+    return 0;
+}
+
+bool cmp(ll a,ll b){
+	return a<b;
+}
+
+int main(){
+	ll n;
+	cin>>n;
+	ll arr[n];
+	for (int i=0;i<n;i++)
+		cin>>arr[i];
+	sort(arr,arr+n,cmp);
+	ll sum = 0;
+	for (int i=0;i<n/2;i++){
+		sum -= 2*arr[i];
+		sum += 2*arr[n-i-1];
+	}
+	cout<<sum<<endl;
+}
